15ea59651c95ea544c13d1c868949d1ae6252482
SamedliSamuray <samedlisamuray@gmail.com>
2024-04-10 03:16:00 +0400

    'com'

---
+++ get_money.js
@@ -0,0 +1,50 @@
+let numb=document.querySelector('.number');   
+let btn=document.querySelector('.btn');
+
+var manats=document.createElement('div');
+manats.classList.add('manats');
+document.body.append(manats);
+
+btn.addEventListener('click',()=>{
+
+    manats.innerHTML='';  
+    
+    number=numb.value;
+
+    
+    let a=[500,200,100,50,20,10,5,1];
+
+    for(let i=0;i<=7;i++){
+        
+        if(number!==0){
+        let manat=document.createElement('div');
+        manat.classList.add('manat');
+
+        let eded=(Math.trunc(number/a[i]));
+        let say=document.createElement('p');
+            say.classList.add('say');
+            eded>5?say.textContent=`x${eded}`:null;
+
+        for(let j=1;j<=eded;j++){
+            
+
+            let img=document.createElement('img');
+            img.classList.add('img')
+            img.src=`${a[i]}.jpg`;
+            img.style.marginLeft=`${20*j}px`;
+            
+            manat.append(img,say);
+            manats.append(manat)
+        }
+    }
+    
+        number=number%a[i];
+          
+    }
+   
+     
+}
+)
+
+
+
